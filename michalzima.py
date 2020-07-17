dc79fb70faa0acd325d078ba8342c0e2639c5f36
Tulex <tulexx@gmail.com>
2020-07-17 17:40:54 +0200

    my portfolio website

---
+++ michalzima.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+app = Flask(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        name = data['name']
+        message = data['message']
+        file = database.write(f'\n{email}, {name}, {message}')
+
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        email = data['email']
+        name = data['name']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,name,message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/#thankyou')
+        except:
+            return 'Not made to db'
+    else:
+        return 'something bad happened'
